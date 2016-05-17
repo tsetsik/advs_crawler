6015a38ce465d536440fa4dc5afe3dbcfb46679a
Tsvetomir Demerdjiev <tsetsik@gmail.com>
2016-05-17 07:50:07 +0300

    add insert/update db action when checking for changes on advertisement

--- db/sqlite.rb
+++ db/sqlite.rb
@@ -7,9 +7,19 @@ class Sqlite
     init_tables
   end
 
-  def find_addv(adapter, id, fields = '*')
+  def find_addv(adapter, id)
     db.results_as_hash = true
-    db.execute("SELECT #{fields} FROM addvs WHERE adapter = ? AND id = ?", [adapter.to_s, id.to_s])
+    db.execute('SELECT * FROM addvs WHERE adapter = ? AND id = ?', [adapter.to_s, id.to_s])[0]
+  end
+
+  def add_addv(adapter, addv)
+    # data = addv.symbolize_keys.slice(:price, :id).merge(adapter: adapter)
+    # db.execute('UPDATE addvs SET price = :price WHERE adapter = :adapter AND id = :id', data)
+  end
+
+  def update_addv(adapter, addv)
+    data = addv.symbolize_keys.slice(:price, :id).merge(adapter: adapter)
+    db.execute('UPDATE addvs SET price = :price WHERE adapter = :adapter AND id = :id', data)
   end
 
   private
--- services/diff_watch_service.rb
+++ services/diff_watch_service.rb
@@ -6,7 +6,7 @@ class DiffWatchService
 
   def call
     @addvs.map do |adapter, addvs|
-      addvs.select { |addv| check_addv(adapter, addv) }
+      addvs.select { |addv| addv_changed?(adapter, addv) }
     end
   end
 
@@ -14,10 +14,23 @@ class DiffWatchService
 
   attr_reader :db
 
-  def check_addv(adapter, addv)
+  def addv_changed?(adapter, addv)
     db_addv = db.find_addv(adapter, addv[:id])
-    puts "\n\n #{db_addv} - db_addv \n\n"
+    return false if db_addv.present? && !price_changed?(db_addv, addv)
+
+    save_change(adapter, db_addv)
     exit
-    false
+    true
+  end
+
+  def save_change(adapter, db_addv)
+    return db.update_addv(adapter, db_addv) if db_addv.present?
+    db.update_addv(adapter, db_addv)
+  end
+
+  def price_changed?(db_addv, addv)
+    fail('Missing addvartisment to check for price') unless db_addv || addv
+
+    db_addv.symbolize_keys[:price] != addv.symbolize_keys[:price]
   end
 end
