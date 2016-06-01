13d2b11232b256b66616543c3452c442620698f9
Tsvetomir Demerdjiev <tsetsik@gmail.com>
2016-06-01 07:11:57 +0300

    refactor notifying service

--- services/diff_watch_service.rb
+++ services/diff_watch_service.rb
@@ -6,7 +6,7 @@ class DiffWatchService
 
   def call
     @advs.map do |adapter, advs|
-      advs.select { |adv| adv_changed?(adapter, adv) }
+      advs.collect { |adv| adv(adapter, adv) }.compact
     end
   end
 
@@ -14,22 +14,25 @@ class DiffWatchService
 
   attr_reader :db
 
-  def adv_changed?(adapter, adv)
+  def adv(adapter, adv)
     db_adv = db.find_adv(adapter, adv[:id])
-    return false if db_adv.present? && !price_changed?(db_adv, adv)
+    return if db_adv.present? && !price_changed?(db_adv.symbolize_keys, adv.symbolize_keys)
 
-    save_change(adapter, db_adv, adv)
-    true
+    save_change(adapter, db_adv, adv.symbolize_keys)
   end
 
   def save_change(adapter, db_adv, adv)
-    return db.update_adv(adapter, adv) if db_adv.present?
-    db.add_adv(adapter, adv)
+    if db_adv.present?
+      db.update_adv(adapter, adv)
+      adv.merge(action: :changed_price, old_price: db_adv.symbolize_keys[:price])
+    else
+      db.add_adv(adapter, adv)
+      adv.merge(action: :new)
+    end
   end
 
   def price_changed?(db_adv, adv)
     fail('Missing advartisment to check for price') unless db_adv || adv
-
-    db_adv.symbolize_keys[:price] != adv.symbolize_keys[:price]
+    db_adv[:price] != adv[:price]
   end
 end
